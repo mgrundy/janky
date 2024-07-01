79c6269d443e615d8fc4f0191958df5116dc3082
Michael Grundy <linuxengr@gmail.com>
2024-07-01 09:23:07 -0400

    Adding stage-view before gutting and re-factoring Should have commited this ages ago
    I didn't want to plop this in because it's a terrible hunk.
    But, it works, so... I probably shouldn't even commit this
    version because it's a rev of the version I've tested extensively
    but is even worse from a code pov.

---
+++ jenkinslight.py
@@ -0,0 +1,98 @@
+'''
+    Ugh. Module
+'''
+
+import json
+from typing import Any
+
+from urllib.parse import urlparse
+from urllib.request import Request, HTTPRedirectHandler, build_opener
+from urllib.parse import quote as urlquote
+from urllib.parse import urlencode
+
+from requests import HTTPError, ConnectionError
+
+from jenkinsapi.utils.requester import Requester
+
+
+class JenkinsLight():
+
+    def __init__(
+        self,
+        baseurl: str,
+        username: str = "",
+        password: str = "",
+        requester=None,
+        ssl_verify: bool = True,
+        cert=None,
+        timeout: int = 10,
+        max_retries=None,
+    ) -> None:
+        """
+        :param baseurl: baseurl for jenkins instance including port, str
+        :param username: username for jenkins auth, str
+        :param password: password for jenkins auth, str
+        :return: a Jenkins obj
+        """
+        self.username = username
+        self.password = password
+        self.baseurl = baseurl
+        if requester is None:
+            requester = Requester
+
+            self.requester = requester(
+                username,
+                password,
+                baseurl=baseurl,
+                ssl_verify=ssl_verify,
+                cert=cert,
+                timeout=timeout,
+                max_retries=max_retries,
+            )
+        else:
+            self.requester = requester
+
+
+    def get_janka_pipeline_data(self, jobname, filename):
+        """_summary_
+
+        Args:
+            j (_type_): _description_
+            jobname (_type_): _description_
+            filename (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """    
+        data = None
+        url = self.baseurl + '/job/' + jobname + '/wfapi/runs'
+        print(f'pipeline url {url}')
+        requester = self.requester
+
+        response = requester.get_url(url)
+
+        content: Any = response.content
+
+        if isinstance(content, str):
+            data = content
+        elif isinstance(content, bytes):
+            data = content.decode(response.encoding or "ISO-8859-1")
+
+        if response.status_code != 200:
+            logger.error(
+                "Failed request at %s with params: %s %s",
+                url,
+                None,
+                "",
+            )
+            response.raise_for_status()
+
+        json_data = json.loads(data)
+
+        if filename is not None:
+            j = json.dumps(json_data, indent=4, ensure_ascii=False)
+            with open(filename, 'w', encoding="utf-8") as output_file:
+                output_file.write(j)
+
+        return json_data
+
---
+++ stage-view.py
@@ -0,0 +1,171 @@
+"""
+Text Mode Jenkins Stage View
+"""
+import argparse
+import configparser
+import json
+import logging
+from typing import Any
+import datetime
+import os
+import sys
+
+#from jenkinsapi.jenkins import Jenkins
+from rich.align import Align
+from rich.console import Console, Group
+from rich.columns import Columns
+from rich.panel import Panel
+from rich.style import Style
+
+from jenkinslight import JenkinsLight
+
+logger = logging.getLogger(__name__)
+
+job_colors = {}
+job_colors["SUCCESS"] = "bright_green"
+job_colors["IN_PROGRESS"] = "yellow"
+job_colors["ABORTED"] = "grey50"
+job_colors["UNSTABLE"] = "bright_red"
+job_colors["FAILED"] = "red3"
+job_colors["NOT_EXECUTED"] = "purple"
+#job_colors[""]
+
+
+
+def main():
+    """
+        main - where the magic happens
+    """
+    opts = parse_commandline()
+
+    # Read the config file and connect to Jenkins
+    (server, uid, token) = load_secrets()
+    #j = Jenkins(server, uid, token)
+    j = JenkinsLight(server, uid, token)
+
+    #buildjob = j[opts.jobname]
+    #buildjob = get_job(j, opts.jobname)
+
+    #view_data = get_pipeline_data(buildjob, opts.filename)
+    view_data = j.get_janka_pipeline_data(j, opts.jobname, opts.filename)
+    console = Console()
+
+    for job in view_data:
+        stages = job["stages"]
+
+        jobtime = datetime.datetime.fromtimestamp(job["startTimeMillis"]/1000.0)
+        date = jobtime.strftime("%b %d")
+        time = jobtime.strftime(" %H:%M")
+        duration = time_str(job["durationMillis"], short=True)
+        statcolor = job_colors[job["status"]]
+        job_string1 = f'[{statcolor}]{job["status"]}'
+        job_string2 = f'[white]{date}'
+        job_string3 = f'[blue]{time}'
+
+        job_renderables = [
+            Panel(
+                Group(
+                    # Align.center(" "),
+                    Align.center(job_string1),
+                    Align.center(job_string2),
+                    Align.center(job_string3),
+                    # Align.center(" "),
+                ),
+                width=15,
+                height=6,
+                title=f'[white]{job["name"]}',
+                subtitle=f"[cyan]{duration}",
+                border_style=statcolor,
+            )
+        ]
+        job_renderables.extend(
+            [
+                Panel(
+                    get_content(stage),
+                    width=15,
+                    height=6,
+                    expand=True,
+                    border_style=job_colors[stage["status"]],
+                )
+                for stage in stages
+            ]
+        )
+
+        console.print(Columns(job_renderables))
+
+def get_job(j, jobname):
+    return j.get_job(jobname)
+
+def time_str(millis, short=False):
+    seconds=(millis/1000)%60
+    seconds = int(seconds)
+    minutes=(millis/(1000*60))%60
+    minutes = int(minutes)
+    hours=(millis/(1000*60*60))%24
+    hours=int(hours)
+    ms_string = f'{minutes:02d}:{seconds:02d}'
+    h_string = f'{hours:02d}:'
+    if short is True:
+        return ms_string
+    return h_string + ms_string
+
+
+def get_content(stage):
+    """Extract text from user dict."""
+    #print(stage)
+    status = stage["status"]
+    name = stage["name"]
+    time = time_str(stage["durationMillis"])
+#    nameA = f"{user['name']['first']} {user['name']['last']}"
+
+    return f'[b][white]{name}[/b]\n[{job_colors[stage["status"]]}]{status}\n[cyan]{time}'
+
+
+def load_secrets():
+    """
+        Reads config file returns (server, userid, personal access token)
+    """
+    cfgfile = 'janky.cfg'
+    if not os.path.isfile(cfgfile):
+        raise ValueError("Config file does not exist")
+    cfg = configparser.ConfigParser()
+    cfg.read(cfgfile)
+    sections = cfg.sections()
+    # Could have a parameter for a specific section, but whatever.
+    sector = sections[0]
+    #['mobilecicd']
+    uname = cfg[sector]['uname']
+    token = cfg[sector]['token']
+    server = cfg[sector]['server']
+
+    return(server, uname, token)
+
+
+def parse_commandline():
+    """
+    Parses command line and returns options object.
+
+        Returns:
+            options (object): Object containing all of the program options set
+            on the command line
+    """
+    parser = argparse.ArgumentParser(prog="launch_build.py", description= """ """)
+
+    # add in command line options
+    parser.add_argument("-f", "--filename", dest="filename",
+                        help="Name of file to save pipeline json to",
+                        default=None)
+    parser.add_argument("-j", "--jobname", dest="jobname",
+                        help="Name of Jenkins job pipeline to view",
+                        default=None)
+
+    options = parser.parse_args()
+
+    if '/' in options.jobname:
+        options.jobname = options.jobname.replace('/', '/job/')
+
+    return options
+
+
+if __name__ == "__main__":
+    main()
